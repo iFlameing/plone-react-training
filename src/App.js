e80cc016a124352261ce6f5e864916dbdba855cd
iFlameing <ialokkumarsingh0@gmail.com>
2021-10-03 00:38:06 +0530

    Finish module 7

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
+import { useState } from "react";
 import "./App.css";
 import FaqItem from "./components/FaqItem";
 
 function App() {
+  const [faqList, setFaqList] = useState([
+    {
+      question: "What does the Plone Foundation do?",
+      answer: "The mission of the Plone Foundation is to protect and...",
+    },
+    {
+      question: "Why does Plone need a Foundation?",
+      answer: "Plone has reached critical mass, with enterprise...",
+    },
+  ]);
+
   return (
     <ul>
-      <FaqItem
-        question="What does the Plone Foundation do?"
-        answer="
-          The mission of the Plone Foundation is to protect and promote Plone.
-          The Foundation provides marketing assistance, awareness, and
-          evangelism assistance to the Plone community. The Foundation also
-          assists with development funding and coordination of funding for
-          large feature implementations. In this way, our role is similar to
-          the role of the Apache Software Foundation and its relationship with
-          the Apache Project."
-      />
-      <FaqItem
-        question="Why does Plone need a Foundation?"
-        answer="
-          Plone has reached critical mass, with enterprise implementations and
-          worldwide usage. The Foundation is able to speak for Plone, and
-          provide strong and consistent advocacy for both the project and the
-          community. The Plone Foundation also helps ensure a level playing
-          field, to preserve what is good about Plone as new participants
-          arrive."
-      />
+      {faqList.map((item) => (
+        <FaqItem question={item.question} answer={item.answer} />
+      ))}
     </ul>
   );
 }
--- src/components/FaqItem.jsx
+++ src/components/FaqItem.jsx
@@ -1,11 +1,13 @@
+import { useState } from "react";
 import "./FaqItem.css";
 import PropTypes from "prop-types";
 
 const FaqItem = (props) => {
+  const [isAnswer, setAnswer] = useState(false);
   return (
     <li className="faq-item">
       <h2 className="question">{props.question}</h2>
-      <p>{props.answer}</p>
+      {isAnswer && <p>{props.answer}</p>}
     </li>
   );
 };
